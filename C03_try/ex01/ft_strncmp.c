37f7c54c789bd82d02fc6b8e215000691356ed4f
Christian Gonzalez Alves <chgonzal@c1r9s8.42porto.com>
2025-01-23 17:26:42 +0000

    Added C03

---
+++ C03_try/ex01/ft_strncmp.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chgonzal <chgonzal@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 11:40:10 by chgonzal          #+#    #+#             */
+/*   Updated: 2025/01/21 11:40:48 by chgonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && i < n)
+	{
+		if (s2[i] != '\0')
+		{
+			if (s1[i] != s2[i])
+			{
+				return (s1[i] - s2[i]);
+			}
+		}
+		else
+		{
+			return (s1[i]);
+		}
+		i++;
+	}
+	if (s2[i] != '\0' && i < n)
+	{
+		return (-s2[i]);
+	}
+	return (0);
+}
