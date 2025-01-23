daee8aa7378e0dc7589a8c87f5a4551fe07e9cfc
Christian Alves <chralves@gmail.com>
2025-01-23 23:27:53 +0000

    Added header

--- C04/ex00/ft_strlen.c
+++ C04/ex00/ft_strlen.c
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chralves <chralves@student.42.fr>          +#+  +:+       +#+        */
+/*   By: chgonzal <chgonzal@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/23 23:03:09 by chgonzal          #+#    #+#             */
-/*   Updated: 2025/01/23 23:08:57 by chralves         ###   ########.fr       */
+/*   Created: 2025/01/23 23:26:10 by chgonzal          #+#    #+#             */
+/*   Updated: 2025/01/23 23:27:30 by chgonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 int	ft_strlen(char *str);
 
 int	ft_strlen(char *str)
