bb114804931ffb06d612c62d99cafbe85cf33e52
Christian Alves <chralves@gmail.com>
2025-01-30 23:40:42 +0000

    Modified ex04

--- C07/ex04/ft_convert_base.c
+++ C07/ex04/ft_convert_base.c
@@ -6,27 +6,13 @@
 /*   By: chgonzal <chgonzal@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 18:20:28 by chgonzal          #+#    #+#             */
-/*   Updated: 2025/01/30 17:06:50 by chgonzal         ###   ########.fr       */
+/*   Updated: 2025/01/30 23:39:34 by chgonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*
 Escreva uma função que devolva o resultado da conversão da string nbr expressa
 de uma base base_from noutra base base_to.
 
-Base from to decimal > Decimal to base to
-
-• nbr, base_from, base_to não serão necessariamente editáveis.
-
-So ler o conteudo e usar variaveis, nao mexer nos pointers...
-
-• nbr seguirá as mesmas regras que ft_atoi_base. Por isso, cuidado com ’+’, ’-’, e
-espaços.
-
-Limpar nbr com as condicoes do atoi base
-
-• O número representado por nbr cabe num int.
-
-Nada de longs nem de testar o min int
 
 • Se uma base estiver incorreta, a função devolverá NULL.
 
@@ -82,6 +68,7 @@ int	check_valid_base(char *str)
 		return (i);
 	return (0);
 }
+
 int	symbol_in_base(char digit, char *base)
 {
 	int	i;
@@ -95,6 +82,22 @@ int	symbol_in_base(char digit, char *base)
 	}
 	return (-1);
 }
+/*
+char	*ft_putnbr_base(int number, int bt, char *base)
+{
+	if (number < 0)
+		write(1, "-", 1);
+	if (abs(number) >= bn)
+	{
+		remainder = abs(number % bn);
+		number = abs(number / bn);
+		ft_putnumber_base(number, base);
+		write(1, &base[remainder], 1);
+		return ;
+	}
+	write(1, &base[number], 1);
+}
+*/
 
 int	get_number_in_base_x(char *nbr, char *base, int bn, int sign, int i)
 {
@@ -110,49 +113,66 @@ int	get_number_in_base_x(char *nbr, char *base, int bn, int sign, int i)
 	}
 	return (number);
 }
-char	*ft_putnbr_base(int number, int bt, char *base)
-{
-	if (nbr < 0)
-		write(1, "-", 1);
-	if (abs(nbr) >= bn)
-	{
-		remainder = abs(nbr % bn);
-		nbr = abs(nbr / bn);
-		ft_putnbr_base(nbr, base);
-		write(1, &base[remainder], 1);
-		return ;
-	}
-	write(1, &base[nbr], 1);
-}
 
+char	*put_number_in_base_x(int nbr, char *base_to, int i, int size)
+{
+	char	*return_nbr;
+	int		base;
+	int		remain;
+
+	base = check_valid_base(base_from);
+	if (base == 0)
+		return (0);
+
+	if (nbr < 0)
+	{
+		size += 1;
+		return_nbr[i] = '-'; // reve esto, porque va a ser la clave...
+		nbr *= -1;
+	}
+	if (nbr >= base)
+	{
+		remain = nbr % base;
+		nbr = nbr / base;
+		put_number_in_base_x(nbr, base_to, i + 1, size + 1);
+		return_nbr[i] = remain;
+	}
+	return_nbr = (char*)malloc(size * sizeof(char));
+	if (return_nbr == NULL || size == 0)
+		return (return_nbr);
+	if (sign == -1)
+		return_nbr[i++] = '-';	
+	return_nbr[i] = nbr;
+	return (return_nbr); // y revisa el retorno...
+}
 
 
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
-	int	bf;
-	int	bt;
+	int	base;
 	int	sign;
 	int	i;
 	int	number;
-	int	to_add;
 	int	ptr_size;
 	char *result;
 
-
 	i = 0;
 	sign = 1;
-	bf = check_valid_base(base_from);
-	if (bf == 0)
+	base = check_valid_base(base_from);
+	if (base == 0)
 		return (0);
 	i = 0;
 	while (is_space(nbr[i]))
 		i++;
 	while (nbr[i] == '-' || nbr[i] == '+')
 		sign = sign * get_sign(nbr[i++]);
-	number = get_number_in_base_x(nbr, base_from, bn, sign, i);
+	number = get_number_in_base_x(nbr, base_from, base, sign, i);
 
-	bt = check_valid_base(base_to);
-	if (bt == 0)
+	printf("The number is %d", sign * number);
+
+	/*
+	base = check_valid_base(base_to);
+	if (base == 0)
 		return (0);
 
 	result = (char*)malloc(sizeof(char) + 1);
@@ -168,16 +188,11 @@ char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 	if (join == NULL || size == 0)
 		return (join);
 
+*/
 
-
-
-
-
-
-	printf("Number is %d\n", number);
 }
 
 int main(void)
 {
-	ft_convert_base("   +++---223", "0123456789", "0123456789");
+	ft_convert_base("   +++---42", "0123456789", "0123456789");
 }
