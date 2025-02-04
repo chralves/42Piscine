136f7a64ec0a1c1d421f1764e18758c88ff8463c
Christian Alves <chralves@gmail.com>
2025-02-04 13:48:04 +0000

    Correction of C03 ex05

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: chgonzal <chgonzal@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 18:28:12 by chgonzal          #+#    #+#             */
-/*   Updated: 2025/01/29 16:09:06 by chgonzal         ###   ########.fr       */
+/*   Updated: 2025/02/04 13:47:11 by chgonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,7 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	src_size = 0;
 	dest_size = 0;
 	while (dest[src_size])
-	{
 		src_size++;
-	}
 	sum = src_size;
 	while (src[dest_size] && src_size < size)
 	{
@@ -34,9 +32,7 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 		src_size++;
 	}
 	if (src_size < size)
-	{
 		dest[src_size] = '\0';
-	}
 	sum += dest_size;
 	return (sum);
 }
