5293cc94536e972acc24687ba3efb9636ba9a53c
chgonzal <chgonzal@student.42porto.com>
2025-02-04 16:00:10 +0000

    Added corrections to all exercises in C03

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -6,11 +6,9 @@
 /*   By: chgonzal <chgonzal@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 12:44:00 by chgonzal          #+#    #+#             */
-/*   Updated: 2025/01/22 12:44:49 by chgonzal         ###   ########.fr       */
+/*   Updated: 2025/02/04 15:58:55 by chgonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-char	*ft_strncat(char *dest, char *src, unsigned int nb);
 
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
@@ -20,15 +18,12 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	i = 0;
 	j = 0;
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
 	while (src[j] != '\0' && j < nb)
 	{
-		dest[i] = src[j];
+		dest[i + j] = src[j];
 		j++;
-		i++;
 	}
-	dest[i] = '\0';
+	dest[i + j] = '\0';
 	return (dest);
 }
